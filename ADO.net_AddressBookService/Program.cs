896b8a8c69953a289aa4e3e302c88990763d3db1
Hemantdhurve <hemantdhurve007@gmail.com>
2022-09-29 17:46:31 +0530

    Welcome Message

---
+++ ADO.net_AddressBookService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.net_AddressBookService
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to ADO.Net Address Book Service Problem");
+        }
+    }
+}
